418d108fc9254e6507ec55e8bf72de096a5abf94
a.sidorov <a.sidorov@bivgroup.ru>
2023-04-04 17:18:05 +0300

    added sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from "./pages/main-page/main-page.component";
+import { NotebookPageComponent } from "./pages/notebook-page/notebook-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    title: 'Главная - ваши заметки',
+    component: MainPageComponent
+  },
+  {
+    path: 'notebook',
+    title: 'Ваши блокноты',
+    component: NotebookPageComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { NotebookPageComponent } from './pages/notebook-page/notebook-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    NotebookPageComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
