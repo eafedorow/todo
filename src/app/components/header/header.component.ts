f05a7ab5a36d632f628d22ca28f3335379345251
a.sidorov <a.sidorov@bivgroup.ru>
2023-04-05 16:10:30 +0300

    header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  date: number;
 
+  ngOnInit(): void {
+    this.date = Date.now();
+  }
 }
